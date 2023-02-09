6fdf053543d5cdbb2d0345b4acb0923819c953e4
codelikeachode <derpypj@gmail.com>
2023-02-09 00:55:18 -0500

    added error handling for the input URL

--- yt-downloader/yt-downloader.py
+++ yt-downloader/yt-downloader.py
@@ -1,23 +1,36 @@
 from pytube import YouTube
+import re
 
-def download_video(url):
+
+def download_youtube_video(url):
+    # Validate input URL format
+    url_regex = re.compile(
+        r'^(https?://)?(www\.)?(youtube\.com|youtu\.?be)/.+$')
+    if not url_regex.match(url):
+        raise ValueError("Invalid URL format")
+
     try:
         # Get the video from the URL
         yt = YouTube(url)
-        
+
         # Get the highest quality video stream available
-        video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
-        
+        video = yt.streams.filter(progressive=True, file_extension='mp4').order_by(
+            'resolution').desc().first()
+
         # Download the video to the current directory
         video.download()
-        
+
         print("Video downloaded successfully")
     except Exception as e:
         print("An error occurred while downloading the video")
         print(e)
-        
+
+
 # Ask the user for the YouTube URL
 url = input("Enter the YouTube URL: ")
 
 # Download the video
-download_video(url)
+try:
+    download_youtube_video(url)
+except ValueError as e:
+    print(e)
